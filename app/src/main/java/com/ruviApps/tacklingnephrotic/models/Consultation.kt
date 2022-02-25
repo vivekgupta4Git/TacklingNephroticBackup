14903cc18165e9e26fd65505a65c8d5a324ae80b
Vivek <itguru4all@gmail.com>
2022-02-25 23:25:58 +0530

    Added Consultation.kt with required Entities.

--- app/src/main/java/com/ruviApps/tacklingnephrotic/models/Consultation.kt
+++ app/src/main/java/com/ruviApps/tacklingnephrotic/models/Consultation.kt
@@ -1,38 +1,62 @@
 package com.ruviApps.tacklingnephrotic.models
 
 import android.telecom.CallAudioState
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.ForeignKey
+import androidx.room.*
 import androidx.room.ForeignKey.Companion.CASCADE
-import androidx.room.PrimaryKey
 import java.util.*
 
 
 @Entity(foreignKeys = [ForeignKey(
     entity = Patient::class,
     parentColumns = ["patient_id"],
-    childColumns = ["patient_consulted_id"],
-    onDelete = ForeignKey.CASCADE
+    childColumns = ["patient_id"],
+    onDelete = CASCADE
 )])
 data class Consultation(
     @PrimaryKey(true)
     @ColumnInfo("consult_id")
     val consultId : Long,
-    @ColumnInfo("patient_consulted_id")
+    @ColumnInfo("patient_id")
     val patientId : Long,
-    val recordedDate : Date
+    @ColumnInfo("visit_date")
+    val visitDate : Date,
+    @ColumnInfo("doctor_consulted_id")
+    val consultedDoctorId : Long
+
 )
+
+
+/**
+ * One to many relationship between patient and consultations
+ */
+
+data class PatientWithConsultations(
+    @Embedded
+val patient : Patient,
+    @Relation(
+        parentColumn = "patient_id",
+        entityColumn = "patient_id"
+    )
+val consultations : List<Consultation>
+)
+
+/**
+ *@Transaction
+ * @Query("Select * from Patient)
+ * fun getPatientWithConsultations(): List<patientWithConsultations>
+ */
+
+/*
 
 @Entity(foreignKeys = [ForeignKey(
     entity = Consultation::class,
     parentColumns = ["consult_id"],
     childColumns = ["consult_key"],
-    onDelete = ForeignKey.CASCADE
+    onDelete = CASCADE
 ), ForeignKey(entity = Doctor::class,
     parentColumns = ["doctor_id"],
     childColumns = ["consulted_doctor_id"],
-    onDelete = ForeignKey.CASCADE)])
+    onDelete = CASCADE)])
 data class ConsultationDetails(
     @PrimaryKey(true)
     @ColumnInfo("consultation_details_id")
@@ -42,19 +66,62 @@ data class ConsultationDetails(
     @ColumnInfo("consulted_doctor_id")
     val doctorId: Long,
 )
+*/
+
+/**
+ * One to one Relationship with between consultation and consultation detials
+ */
+/*
+data class ConsultationAndDetails(
+    @Embedded
+    val consultation: Consultation,
+    @Relation(
+        parentColumn = "consult_id",
+        entityColumn = "consult_key"
+    )
+    val details : ConsultationDetails
+)
+*/
+
+/**
+ * @Transaction
+ * @Query("Select * from Consultation)
+ * fun getConsultationAndDetails() : List<ConsultationsAndDetails>
+ */
+
+
+// one to one relationship between Consultation and Doctor.
+data class DoctorWithConsultations(
+    @Embedded val doctor: Doctor,
+    @Relation(
+        parentColumn = "doctor_id",
+        entityColumn = "doctor_consulted_id"
+    )
+    val consultations: List<Consultation>
+)
+
+/**
+ * @Transaction
+ * @Query("Select * from Doctor")
+ * fun getDoctorWithConsultations() : List<DoctorWithConsultations>
+ */
+
 
 @Entity(foreignKeys = [ForeignKey(
-    entity = ConsultationDetails::class,
-    parentColumns = ["consultation_details_id"],
-    childColumns = ["consult_details_id"],
+    entity = Consultation::class,
+    parentColumns = ["consult_id"],
+    childColumns = ["consult_id"],
     onDelete = CASCADE
 )])
 data class PrescriptionDetails(
     @PrimaryKey(true)
     @ColumnInfo("prescription_id")
     val prescriptionId : Long,
-    @ColumnInfo("consult_details_id")
-    val consultDetailsID:Long,
+    @ColumnInfo("consult_id")
+    val consultID:Long,
+
+    @ColumnInfo("prescription_for_diseases_id")
+    val diseaseId : Long,
 
     val complaints : String?,
     val diagnosis : String?,
@@ -64,6 +131,43 @@ data class PrescriptionDetails(
     val nextFollowUpDate : Date
 
 )
+
+/**
+ * In each Consultation  , there can be many prescription details for different diseases
+ * so one to many relationship between Consultation  and Prescription Details
+ */
+data class ConsultationWithPrescriptionDetails(
+    @Embedded
+    val consultation: Consultation,
+    @Relation(
+        parentColumn = "consult_id",
+        entityColumn = "consult_id"
+    )
+    val prescriptionDetails: List<PrescriptionDetails>
+)
+
+/**
+ * @Transaction
+ * @Query("Select * from ConsultationDetails")
+ * fun getConsultationWithPrescriptionDetails() : List<ConsultationWithPrescriptionDetails>
+ */
+
+
+/**
+ * There can be one Prescription Details and it can have multiple Medicine Details,
+ * So one to many
+ *
+ * One medicine Unit can be in multiple Prescription Details
+ * so one to many
+ * similarly
+ * One frequency unit can be in multiple prescription Details
+ * so one to many
+ *
+ *And also,
+ * one medicine can be in multiple prescription details
+ * so one to many
+ */
+
 
 @Entity(foreignKeys = [ForeignKey(
     entity = PrescriptionDetails::class,
@@ -109,3 +213,54 @@ data class MedicineDetails(
     @ColumnInfo("medicine_frequency_code")
     val medicineFrequency: Long
     )
+
+
+data class PrescriptionDetailsWithMedicineDetails(
+    @Embedded
+    val prescriptionDetails: PrescriptionDetails,
+    @Relation(
+        parentColumn = "prescription_id",
+        entityColumn = "prescribed_details_id"
+    )
+    val medicine_details : List<MedicineDetails>
+)
+
+/**
+ * @Transaction
+ * @Query("Select * from PrescriptionDetails")
+ * fun getPrescriptionDetailsWithMedicineDetails():
+ * List<PrescriptionDetailsWithMedicineDetails>
+ */
+
+data class MedicineWithPrescriptionDetails(
+    @Embedded val medicine: Medicine,
+    @Relation(
+        parentColumn = "medicine_code",
+        entityColumn = "prescribed_medicine_id"
+    )
+    val prescriptionDetails : List<PrescriptionDetails>
+)
+/**
+ * @Transaction
+ * @Query("Select * from medicine")
+ * fun getMedicineWithPrescriptionDetails():
+ * List<MedicineWithPrescriptionDetails>
+ */
+
+data class MedicineUnitWithPrescriptionDetails(
+    @Embedded val medicineUnit: MedicineUnit,
+    @Relation(
+        parentColumn = "unit_id",
+        entityColumn = "medicine_unit_code"
+    )
+    val prescriptionDetails: List<PrescriptionDetails>
+)
+
+data class FrequencyWithPrescriptionDetails(
+    @Embedded val medicineFrequency: Frequency,
+    @Relation(
+        parentColumn = "frequency_code",
+        entityColumn = "medicine_frequency_code"
+    )
+    val prescriptionDetails: List<PrescriptionDetails>
+)
