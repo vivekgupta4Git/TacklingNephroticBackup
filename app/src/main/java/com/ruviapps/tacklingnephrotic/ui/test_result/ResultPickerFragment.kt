e195b695c1828bc59459b6ad61cf83d7c2b0289b
Vivek <itguru4all@gmail.com>
2022-06-23 17:48:22 +0530

    Working on UI  First Phase in progress  --Added reading slider with compare dipstick result feature

--- app/src/main/java/com/ruviapps/tacklingnephrotic/ui/test_result/ResultPickerFragment.kt
+++ app/src/main/java/com/ruviapps/tacklingnephrotic/ui/test_result/ResultPickerFragment.kt
@@ -1,48 +1,206 @@
 package com.ruviapps.tacklingnephrotic.ui.test_result
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.cardview.widget.CardView
+import androidx.compose.animation.core.animateIntOffsetAsState
+import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import com.ruviapps.tacklingnephrotic.R
-import com.ruviapps.tacklingnephrotic.database.entities.ResultCode
-import com.ruviapps.tacklingnephrotic.databinding.ResultPickerFragmentBinding
-import com.ruviapps.tacklingnephrotic.domain.TestResult
-import com.ruviapps.tacklingnephrotic.ui.home.HomeFragment
-import com.ruviapps.tacklingnephrotic.utility.Event
-import com.ruviapps.tacklingnephrotic.utility.EventObserver
-import com.ruviapps.tacklingnephrotic.utility.NavigationCommand
+import androidx.lifecycle.lifecycleScope
+import com.ruviapps.tacklingnephrotic.BuildConfig
+import com.ruviapps.tacklingnephrotic.databinding.ReadingSliderBinding
 import dagger.hilt.android.AndroidEntryPoint
-import java.time.*
+import kotlinx.coroutines.launch
+import java.io.File
+import java.util.*
 
 
 @AndroidEntryPoint
 class ResultPickerFragment : Fragment() {
 
-    val viewModel : ResultPickerViewModel by viewModels()
+    private var latestImageUri : Uri? = null
+    private val firstCardPreviewImage by lazy { binding.firsCardUserImage }
+    private val secondCardPreviewImage by lazy { binding.secondCardUserImage }
+    private val thirdCardPreviewImage by lazy { binding.thirdCardUserImage }
+    private val forthCardPreviewImage by lazy { binding.fourthCardUserImage }
+    private val fifthCardPreviewImage by lazy { binding.fifthCardUserImage }
+    private val sixthCardPreviewImage by lazy { binding.SixthCardUserImage }
+    private val readingContainer by lazy {  binding.readingsContainer}
+    private val firstCardView by lazy {   binding.firstCard }
+    private val secondCardView by lazy {binding.secondCard }
+    private val thirdCardView by lazy { binding.thirdCard }
+    private val forthCardView by lazy {binding.fourthCard }
+    private val fifthCardView by lazy {binding.fifthCard}
+    private val sixthCardView by lazy {binding.SixthCard}
+    private   val sliderContainer by lazy {binding.sliderContainer}
 
-    private lateinit var binding : ResultPickerFragmentBinding
 
+    private val takePictureContract = registerForActivityResult(ActivityResultContracts.TakePicture()){
+            taken->
+        if(taken){
+            latestImageUri.let {
+                firstCardPreviewImage.visibility = View.VISIBLE
+                secondCardPreviewImage.visibility = View.VISIBLE
+                thirdCardPreviewImage.visibility = View.VISIBLE
+                forthCardPreviewImage.visibility = View.VISIBLE
+                fifthCardPreviewImage.visibility = View.VISIBLE
+                sixthCardPreviewImage.visibility = View.VISIBLE
+                firstCardPreviewImage.setImageURI(it)
+                secondCardPreviewImage.setImageURI(it)
+                thirdCardPreviewImage.setImageURI(it)
+                forthCardPreviewImage.setImageURI(it)
+                fifthCardPreviewImage.setImageURI(it)
+                sixthCardPreviewImage.setImageURI(it)
+
+            }
+        }else
+        {
+            firstCardPreviewImage.visibility  = View.GONE
+            secondCardPreviewImage.visibility = View.GONE
+            thirdCardPreviewImage.visibility = View.GONE
+            forthCardPreviewImage.visibility = View.GONE
+            fifthCardPreviewImage.visibility = View.GONE
+            sixthCardPreviewImage.visibility = View.GONE
+        }
+    }
+    val viewModel: ResultPickerViewModel by viewModels()
+    private lateinit var binding: ReadingSliderBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        binding = ResultPickerFragmentBinding.inflate(layoutInflater)
-      //  viewModel.initializeDatabase()
+        binding = ReadingSliderBinding.inflate(layoutInflater)
+        //  viewModel.initializeDatabase()
         return binding.root
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val container = binding.constraintLayoutContainer
+        container.setOnTouchListener{ _, event->
 
+            when(event.action and event.actionMasked){
+                MotionEvent.ACTION_MOVE ->{
+                    move(event)
+                }
+
+
+            }
+            true
+        }
+
+        val cameraButton = binding.cameraButton
+        cameraButton.setOnClickListener {
+            takePicture()
+        }
+
+
+    }
+
+  private fun move(e : MotionEvent){
+      val maxUpwardMovement = readingContainer.top
+      val minDownwardMovement = readingContainer.top + readingContainer.height
+      val offset = sliderContainer.height/2
+      val firstCardYPos = readingContainer.top
+      val secondCardYPos = firstCardYPos + firstCardView.height
+      val thirdCardYPos = secondCardYPos + secondCardView.height
+      val forthCardYPos = thirdCardYPos + thirdCardView.height
+      val fifthCardYPos = forthCardYPos + forthCardView.height
+      val sixthCardYPos = fifthCardYPos + fifthCardView.height
+
+      when(e.y.toInt()){
+          in maxUpwardMovement..minDownwardMovement -> {
+              sliderContainer.y = e.y - offset
+              Log.d("TAG","${sliderContainer.y}, $firstCardYPos $secondCardYPos")
+
+              when(e.y.toInt()){
+                  in firstCardYPos..secondCardYPos ->  {
+                      firstCardView.isActivated = true
+                      secondCardView.isActivated = false
+                      thirdCardView.isActivated = false
+                      forthCardView.isActivated = false
+                      fifthCardView.isActivated= false
+                      sixthCardView.isActivated = false
+                  }
+                  in secondCardYPos..thirdCardYPos ->   {
+                      firstCardView.isActivated = false
+                      secondCardView.isActivated = true
+                      thirdCardView.isActivated = false
+                      forthCardView.isActivated = false
+                      fifthCardView.isActivated= false
+                      sixthCardView.isActivated = false
+                  }
+                  in thirdCardYPos..forthCardYPos ->    {
+                      firstCardView.isActivated = false
+                      secondCardView.isActivated = false
+                      thirdCardView.isActivated = true
+                      forthCardView.isActivated = false
+                      fifthCardView.isActivated= false
+                      sixthCardView.isActivated = false
+                  }
+                  in forthCardYPos..fifthCardYPos ->    {
+                      firstCardView.isActivated = false
+                      secondCardView.isActivated = false
+                      thirdCardView.isActivated = false
+                      forthCardView.isActivated = true
+                      fifthCardView.isActivated= false
+                      sixthCardView.isActivated = false
+                  }
+                  in fifthCardYPos..sixthCardYPos ->    {
+                      firstCardView.isActivated = false
+                      secondCardView.isActivated = false
+                      thirdCardView.isActivated = false
+                      forthCardView.isActivated = false
+                      fifthCardView.isActivated= true
+                      sixthCardView.isActivated = false
+                  }
+                  in sixthCardYPos..minDownwardMovement -> {
+                      firstCardView.isActivated = false
+                      secondCardView.isActivated = false
+                      thirdCardView.isActivated = false
+                      forthCardView.isActivated = false
+                      fifthCardView.isActivated= false
+                      sixthCardView.isActivated = true
+                  }
+
+              }
+
+          }
+    }
+
+
+
+    }
+
+    private fun takePicture(){
+        lifecycleScope.launch {
+            getTmpPicture().let {
+                latestImageUri = it
+                takePictureContract.launch(it)
+            }
+        }
+    }
+    private fun getTmpPicture() : Uri?{
+        val tmpFile = File.createTempFile(Calendar.getInstance().timeInMillis.toString(),".jpeg",requireContext().cacheDir).apply {
+            createNewFile()
+            deleteOnExit()
+        }
+        return   FileProvider.getUriForFile(requireContext(), "${BuildConfig.APPLICATION_ID}.provider", tmpFile)
+    }
+
+
+/* binding.arrow.setOnTouchListener(touchListener)
         viewModel.navigateToDashBoard.observe(viewLifecycleOwner) { event ->
             event.getContentIfNotHandled()?.let { command ->
                 when(command){
@@ -56,8 +214,9 @@ class ResultPickerFragment : Fragment() {
             }
         }
 
-
-        binding.result1PlusBtn.setOnClickListener {
+        binding.arrow.y = binding.resultNegativePlusBtn.y
+*/
+        /*binding.result1PlusBtn.setOnClickListener {
             viewModel.saveResult(TestResult(LocalDate.now(),
                 ResultCode.ONE_PLUS.name,
                 "",
@@ -90,6 +249,89 @@ class ResultPickerFragment : Fragment() {
                 "",1))
 
         }
-
+*/
     }
-}
+
+
+
+
+   /* private val touchListener = OnTouchListener { view, event ->
+        val x = event.rawX.toInt()
+        val y = event.rawY.toInt()
+
+
+        when (event.action and MotionEvent.ACTION_MASK) {
+
+
+
+            MotionEvent.ACTION_DOWN -> {
+                val lParams = view.layoutParams as FrameLayout.LayoutParams
+                xDelta = x - lParams.leftMargin
+                yDelta = y - lParams.topMargin
+            }
+            MotionEvent.ACTION_UP -> {
+                val lparas = view.layoutParams as FrameLayout.LayoutParams
+                xDelta = x - lparas.leftMargin
+                yDelta = y- lparas.bottomMargin
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                if (y - yDelta + view.height <= container.getHeight() && y - yDelta >= 0) {
+                val layoutParams = view.layoutParams as FrameLayout.LayoutParams
+                layoutParams.topMargin = y - yDelta
+                layoutParams.bottomMargin = 0
+                view.layoutParams = layoutParams
+            }
+
+              *//* if (x - xDelta + view.width <= container.getWidth() && y - yDelta + view.height <= container.getHeight() && x - xDelta >= 0 && y - yDelta >= 0) {
+                    val layoutParams = view.layoutParams as FrameLayout.LayoutParams
+                    layoutParams.leftMargin = x - xDelta
+                    layoutParams.topMargin = y - yDelta
+                    layoutParams.rightMargin = 0
+                    layoutParams.bottomMargin = 0
+                    view.layoutParams = layoutParams
+                }*//*
+            }
+
+            MotionEvent.ACTION_BUTTON_PRESS ->{
+
+            }
+
+
+        }
+        container.invalidate()
+        true
+    }
+
+}*/
+
+/*
+//use of compose
+
+override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?,
+): View {
+//below line is important for fragment; In activity we can simply call setContent() but not in fragment
+    return ComposeView(requireContext()).apply {
+
+        setContent {
+            Column(verticalArrangement = Arrangement.Center,
+
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()) {
+                ResultButton(text = "NEGATIVE", viewModel = viewModel)
+                ResultButton(text = "ONE_PLUS", viewModel = viewModel)
+                ResultButton(text = "TWO_PLUS", viewModel = viewModel)
+                ResultButton(text = "THREE_PLUS", viewModel = viewModel)
+                ResultButton(text = "FOUR_PLUS", viewModel = viewModel)
+
+            }
+
+        }
+    }
+
+//Result button is a composable function
+ */
+
