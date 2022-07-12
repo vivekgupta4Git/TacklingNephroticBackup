c2ac2106ee1eda78a3f471e13c84ccec2651e435
Vivek <itguru4all@gmail.com>
2022-07-13 02:33:40 +0530

    Facebook part 1 complete

--- app/src/main/java/com/ruviapps/tacklingnephrotic/LoginActivity.kt
+++ app/src/main/java/com/ruviapps/tacklingnephrotic/LoginActivity.kt
@@ -1,5 +1,7 @@
 package com.ruviapps.tacklingnephrotic
 
+import android.content.Intent
+import android.content.res.Resources
 import android.graphics.Color
 import android.os.Bundle
 import android.text.Spannable
@@ -8,25 +10,55 @@ import android.text.style.ForegroundColorSpan
 import android.text.style.UnderlineSpan
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.registerForActivityResult
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textview.MaterialTextView
 import com.ruviapps.tacklingnephrotic.ui.BottomSheet
 
 
 class LoginActivity : AppCompatActivity() {
 
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ){
+        result->
+        handleResult(result)
+    }
+    private  val providers = arrayListOf( AuthUI.IdpConfig.EmailBuilder().build())
+
+    private fun handleResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if(result.resultCode == RESULT_OK){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }else{
+            if(response===null){
+                    when(response?.error?.errorCode){
+                        ErrorCodes.NO_NETWORK -> Toast.makeText(this,getString(R.string.no_internet_exclaim),Toast.LENGTH_LONG).show()
+                        ErrorCodes.UNKNOWN_ERROR -> Toast.makeText(this,getString(R.string.unknown_error_with_error_code)
+                                +  "${response.error?.errorCode}",Toast.LENGTH_SHORT).show()
+                        else-> Toast.makeText(this,"Login Failed  ",Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContentView(R.layout.activity_login)
 
-        val signIn = findViewById<TextView>(R.id.signIn)
+        val signIn = findViewById<MaterialTextView>(R.id.signIn)
         val wordTwo: Spannable = SpannableString(getString(R.string.sign_in))
 
      /*   wordTwo.setSpan(ForegroundColorSpan(R.color.md_theme_light_onPrimary),
@@ -40,9 +72,18 @@ class LoginActivity : AppCompatActivity() {
         signIn.append(wordTwo)
         signIn.isClickable = true
 
-        val getStartedButton = findViewById<Button>(R.id.getStartedButton)
+        val getStartedButton = findViewById<MaterialButton>(R.id.getStartedButton)
         getStartedButton.setOnClickListener {
-
+            val signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setIsSmartLockEnabled(false)       //for testing purpose
+                .setLogo(R.mipmap.ic_launcher)
+                .setTheme(R.style.Theme_MyLoginTheme)
+                .setAvailableProviders(providers)
+                .build()
+            lifecycleScope.launchWhenStarted {
+                signInLauncher.launch(signInIntent)
+            }
         }
 
         signIn.setOnClickListener {
--- app/src/main/java/com/ruviapps/tacklingnephrotic/ui/BottomSheet.kt
+++ app/src/main/java/com/ruviapps/tacklingnephrotic/ui/BottomSheet.kt
@@ -1,18 +1,25 @@
 package com.ruviapps.tacklingnephrotic.ui
 
 import android.content.Intent
-import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.facebook.*
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.snackbar.Snackbar
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
-import com.ruviapps.tacklingnephrotic.LoginActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.ruviapps.tacklingnephrotic.MainActivity
 import com.ruviapps.tacklingnephrotic.R
 import com.ruviapps.tacklingnephrotic.databinding.FragmentBottomSheetBinding
@@ -22,9 +29,14 @@ import com.ruviapps.tacklingnephrotic.databinding.FragmentBottomSheetBinding
  * Use the [BottomSheet.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class BottomSheet : BottomSheetDialogFragment() {
 
+    private lateinit var auth: FirebaseAuth
+    private val callbackManager = CallbackManager.Factory.create()
+
     private lateinit var binding : FragmentBottomSheetBinding
+
     private val providers = arrayListOf(
         AuthUI.IdpConfig.EmailBuilder().build(),
         AuthUI.IdpConfig.PhoneBuilder().build(),
@@ -34,8 +46,7 @@ class BottomSheet : BottomSheetDialogFragment() {
         FirebaseAuthUIActivityResultContract()
     ){
             result ->
-        run {
-            if(result!=null) {
+        if(result!=null) {
                 startActivity(Intent(requireActivity(), MainActivity::class.java))
                 dismiss()
                 requireActivity().finish()
@@ -45,13 +56,17 @@ class BottomSheet : BottomSheetDialogFragment() {
                 dismiss()
             }
 
-        }
+
     }
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentBottomSheetBinding.inflate(layoutInflater)
+        auth = Firebase.auth
+        FacebookSdk.sdkInitialize(requireContext())
         // Inflate the layout for this fragment
         return binding.root
     }
@@ -68,7 +83,52 @@ class BottomSheet : BottomSheetDialogFragment() {
 
        }
 
+       val faceBookLoginButton = binding.facebookBtn
+       faceBookLoginButton.setReadPermissions("email", "public_profile")
+
+       faceBookLoginButton.registerCallback(callbackManager,object :
+                FacebookCallback<LoginResult> {
+                override fun onSuccess(loginResult: LoginResult) {
+                    Log.d("facebook", "facebook:onSuccess:$loginResult")
+                    handleFacebookAccessToken(loginResult.accessToken)
+                }
+
+                override fun onCancel() {
+                    Log.d("facebook", "facebook:onCancel")
+                }
+
+                override fun onError(error: FacebookException) {
+                    Log.d("facebook", "facebook:onError", error)
+                }
+            })
+
+
        }
 
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d("facebook", "handleFacebookAccessToken:$token")
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("facebook", "signInWithCredential:success")
+                    val user = auth.currentUser
+                    Log.d("facebook","User = $user")
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("facebook", "signInWithCredential:failure", task.exception)
+                    Toast.makeText(requireContext(), "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    Log.d("facebook","Error login using facebook")
+                }
+            }
+    }
 }
